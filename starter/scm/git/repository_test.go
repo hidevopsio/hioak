12e0a5ac30f3c9119d72272923a2dfbd19646166
Wang Lu Lu <wang.lulu@vpclub.cn>
2018-10-09 11:05:43 +0800

    update git  field

--- starter/scm/git/repository_test.go
+++ starter/scm/git/repository_test.go
@@ -4,6 +4,9 @@ import (
 	"fmt"
 	"github.com/stretchr/testify/assert"
 	"gopkg.in/src-d/go-git.v4"
+	"gopkg.in/src-d/go-git.v4/plumbing"
+	"gopkg.in/src-d/go-git.v4/plumbing/transport"
+	"gopkg.in/src-d/go-git.v4/plumbing/transport/http"
 	"testing"
 )
 
@@ -42,14 +45,12 @@ func TestClone(t *testing.T) {
 
 	for _, testCase := range testCases {
 		t.Run(testCase.title, func(t *testing.T) {
-			path, err := repo.Clone(
-				testCase.url,
-				testCase.branch,
-				testCase.dstDir,
-				testCase.username,
-				testCase.password,
-			)
-
+			path, err := repo.Clone(&git.CloneOptions{URL:testCase.url,
+			ReferenceName:plumbing.Master,
+			RecurseSubmodules: git.DefaultSubmoduleRecursionDepth,
+			Auth:transport.AuthMethod(&http.BasicAuth{
+				Username: testCase.username,
+				Password: testCase.password})},testCase.dstDir)
 			assert.Equal(t, testCase.expectedPath, path)
 			assert.Equal(t, testCase.err, err)
 		})
@@ -63,6 +64,6 @@ func TestCloneErr(t *testing.T) {
 		return nil, fmt.Errorf("clone faile")
 	})
 
-	_, err := repo.Clone("", "", "", "", "")
+	_, err := repo.Clone(&git.CloneOptions{}, "")
 	assert.Equal(t, err, fmt.Errorf("clone faile"))
 }
