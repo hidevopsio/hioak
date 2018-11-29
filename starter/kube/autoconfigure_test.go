38a3ed5437c78febec97d3375f8cf894da1322c0
chulei <chu.leilei@vpclub.cn>
2018-11-29 10:34:03 +0800

    update ignored exception

--- starter/kube/autoconfigure_test.go
+++ starter/kube/autoconfigure_test.go
@@ -2,10 +2,12 @@ package kube
 
 import (
 	"github.com/prometheus/common/log"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
 	"testing"
 )
 
-func TestNewConfigMaps(t *testing.T) {
+func TestAutoConfigure(t *testing.T) {
 	c := newConfiguration()
 	cm := c.ConfigMaps(nil)
 	log.Info(cm)
@@ -20,5 +22,27 @@ func TestNewConfigMaps(t *testing.T) {
 	deployment := c.Deployment(nil)
 	log.Info(deployment)
 	replicationController := c.ReplicationController(nil)
+	log.Info(replicationController)
+}
+
+
+func TestNewAutoConfigure(t *testing.T) {
+	c := newConfiguration()
+	clientSet, _ := kubernetes.NewForConfig(&rest.Config{})
+	cm := c.ConfigMaps(clientSet)
+	log.Info(cm)
+	pod := c.Pod(clientSet)
+	log.Info(pod)
+	secret := c.Secret(clientSet)
+	log.Info(secret)
+	service := c.Service(clientSet)
+	log.Info(service)
+	replicaSet := c.ReplicaSet(clientSet)
+	log.Info(replicaSet)
+	deployment := c.Deployment(clientSet)
+	log.Info(deployment)
+	token := c.Token(nil)
+	log.Info(token)
+	replicationController := c.ReplicationController(clientSet)
 	log.Info(replicationController)
 }
